81ee5c5580379addff8f7e1223c54b99b2543a1b
MANAL ABDUL GAFOOR <manalgafoor24@gmail.com>
2022-02-09 12:43:04 +0000

    Update file storage

--- main.py
+++ main.py
@@ -10,14 +10,16 @@ def home():
 
 
 @app.route("/answer", methods=["POST"])
-def calculate():
-    with open(request.form["filename"]) as file:
-        contents = file.readlines()
-    numbers_list = [int(item.strip()) for item in contents]
-    frequency_dict = collections.Counter(numbers_list)
-    new_list = [item[0] for item in list(frequency_dict.most_common(5))]
-    # return f"<h1>The most frequent numbers are: {new_list}</h1>"
-    return render_template("answer.html", list=new_list)
+def find_freq():
+    if request.method == "POST":
+        f = request.files["filename"]
+        f.save(f.filename)
+        with open(f.filename) as file:
+            contents = file.readlines()
+        numbers_list = [int(item.strip()) for item in contents]
+        frequency_dict = collections.Counter(numbers_list)
+        new_list = [item[0] for item in list(frequency_dict.most_common(5))]
+        return render_template("answer.html", list=new_list)
 
 
 if __name__ == "__main__":
